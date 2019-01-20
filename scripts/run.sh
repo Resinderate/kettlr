46a9e818fa6250772d1342128c2a1984fdff36c4
Ronan Murphy <ronan.murphy@originmarkets.com>
2019-01-20 22:48:56 +0000

    New path on kettlr and fixing typo.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "`dirname $0`/.."
 git pull
-pip install -r requirments.txt
+pip install -r requirements.txt
 
-export FLASK_APP=/home/pi/dev/kettlr.py
-flask run --host=0.0.0.0
+export FLASK_APP=/home/pi/dev/kettlr/kettlr.py
+flask run --host=0.0.0.0 --port=5000
